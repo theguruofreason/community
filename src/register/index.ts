99d5916394bf988cc229a204fbde5a7812f927af
James Iden Busia <theguruofreason@gmail.com>
2024-07-15 22:53:10 -0700

    add unregister

--- src/register/index.ts
+++ src/register/index.ts
@@ -19,8 +19,17 @@ router.get("/", (_, res: Response) => {
     const email: string = req.body.email || "";
     const uname: string = req.body.uname;
     const pass: string = req.body.pass;
-    const registerResult : RegisterResult = await register(uname, pass, email);
-    res.status(registerResult.status).send(registerResult.message);
+    if (!uname || !pass) {
+        res.status(400).send("Username and password required.");
+        return;
+    }
+    const result: RegisterResult = await register(uname, pass, email);
+    res.status(result.status).send(result.message);
+})
+.post("/u", async (req: Request, res: Response) => {
+    const uname: string = req.body.uname;
+    const result: RegisterResult = await unregister(uname);
+    res.status(result.status).send(result.message);
 })
 
 type RegisterResult = {
@@ -77,5 +86,43 @@ async function register(uname: string, pass: string, email: string) : Promise<Re
     return {
         status: 200,
         message: `User ${uname} registered!`
+    }
+}
+
+async function unregister(uname: string) : Promise<RegisterResult> {
+    const db = await getLoginDb();
+    if (!db) {
+        return {
+            status: 500,
+            message: "Failed to load login db"
+        };
+    }
+    try {
+        const stmt = `SELECT * FROM ${LOGIN_TABLE} WHERE uname=:uname`;
+        const result = await db.get(stmt, {
+            ":uname": uname
+        })
+        if (!result) {
+            return {
+                status: 400,
+                message: `Username ${uname} not registered...`
+            }
+        }
+    } catch (e) {
+        console.log(e);
+        return DB_FAILURE;
+    }
+    try {
+        const stmt = `DELETE FROM ${LOGIN_TABLE} WHERE uname=:uname`;
+        db.run(stmt, {
+            ":uname": uname
+        })
+    } catch (e) {
+        console.log(e);
+        return DB_FAILURE;
+    }
+    return {
+        status: 200,
+        message: `User ${uname} unregistered.`
     }
 }
