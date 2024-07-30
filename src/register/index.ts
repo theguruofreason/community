30ca615547a9d8a190109fa6bc3e48d4092f4105
Iden Busia <idenbusia@gmail.com>
2024-07-30 11:45:39 -0700

    WIP n4j query

--- src/register/index.ts
+++ src/register/index.ts
@@ -3,129 +3,143 @@ import Router, { Response, Request } from "express";
 import path from "path";
 import { getLoginDb, DB_FAILURE } from "../db";
 import { NEO4J_DRIVER_NAME, SALT_ROUNDS } from "../helpers/configs";
+import { Driver } from "neo4j-driver";
 
 const { LOGIN_TABLE } = process.env;
 export const router = Router();
 
 router
-  .get("/", (_, res: Response) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-  })
-  .post("/", async (req: Request, res: Response) => {
-    const neo4jDriver = req.app.get(NEO4J_DRIVER_NAME);
-    const email: string = req.body.email || "";
-    const uname: string = req.body.uname;
-    const pass: string = req.body.pass;
-    if (!uname || !pass) {
-      res.status(400).send("Username and password required.");
-      return;
-    }
-    const result: RegisterResult = await register(uname, pass, email);
-    res.status(result.status).send(result.message);
-  })
-  .post("/u", async (req: Request, res: Response) => {
-    const uname: string = req.body.uname;
-    const result: RegisterResult = await unregister(uname);
-    res.status(result.status).send(result.message);
-  });
+    .get("/", (_, res: Response) => {
+        res.sendFile(path.join(__dirname, "index.html"));
+    })
+    .post("/", async (req: Request, res: Response) => {
+        const neo4jDriver: Driver = req.app.get(NEO4J_DRIVER_NAME);
+        const email: string = req.body.email || "";
+        const uname: string = req.body.uname;
+        const pass: string = req.body.pass;
+        if (!uname || !pass) {
+            res.status(400).send("Username and password required.");
+            return;
+        }
+        const result: RegisterResult = await register(
+            uname,
+            pass,
+            email,
+            neo4jDriver
+        );
+        res.status(result.status).send(result.message);
+    })
+    .post("/u", async (req: Request, res: Response) => {
+        const uname: string = req.body.uname;
+        const result: RegisterResult = await unregister(uname);
+        res.status(result.status).send(result.message);
+    });
 
 type RegisterResult = {
-  status: number;
-  message: string;
+    status: number;
+    message: string;
 };
 
 async function register(
-  uname: string,
-  pass: string,
-  email: string
+    uname: string,
+    pass: string,
+    email: string,
+    n4jDriver: Driver
 ): Promise<RegisterResult> {
-  const db = await getLoginDb();
-  if (!db) {
-    return {
-      status: 500,
-      message: "Failed to load login db",
-    };
-  }
-  try {
-    const stmt = `SELECT * FROM ${LOGIN_TABLE} WHERE uname=:uname`;
-    const result = await db.get(stmt, {
-      ":uname": uname,
-    });
-    if (result) {
-      return {
-        status: 400,
-        message: `Username ${uname} already registered...`,
-      };
-    }
-  } catch (e) {
-    console.log(e);
-    return DB_FAILURE;
-  }
-  try {
-    bcrypt.hash(pass, SALT_ROUNDS, function (err, hash) {
-      if (err) {
-        throw err;
-      }
-      const insertStmt = `INSERT INTO ${LOGIN_TABLE} (uname, email, password) VALUES (:uname, :email, :password)`;
-      try {
-        db.run(insertStmt, {
-          ":uname": uname,
-          ":email": email,
-          ":password": hash,
-        });
-      } catch (e) {
-        console.log(e);
-        throw DB_FAILURE;
-      }
-    });
-  } catch (e) {
-    return e == DB_FAILURE
-      ? DB_FAILURE
-      : {
-          status: 500,
-          message: (e as Error).message,
+    const db = await getLoginDb();
+    if (!db) {
+        return {
+            status: 500,
+            message: "Failed to load login db",
         };
-  }
-  return {
-    status: 200,
-    message: `User ${uname} registered!`,
-  };
+    }
+    try {
+        const stmt = `SELECT * FROM ${LOGIN_TABLE} WHERE uname=:uname`;
+        const result = await db.get(stmt, {
+            ":uname": uname,
+        });
+        if (result) {
+            return {
+                status: 400,
+                message: `Username ${uname} already registered...`,
+            };
+        }
+    } catch (e) {
+        console.log(e);
+        return DB_FAILURE;
+    }
+    try {
+        bcrypt.hash(pass, SALT_ROUNDS, async function (err, hash) {
+            if (err) {
+                throw err;
+            }
+            const insertStmt = `INSERT INTO ${LOGIN_TABLE} (uname, email, password) VALUES (:uname, :email, :password)`;
+            try {
+                db.run(insertStmt, {
+                    ":uname": uname,
+                    ":email": email,
+                    ":password": hash,
+                });
+                const n4jResult = await n4jDriver.executeQuery(
+                    "MERGE (p:Person {uname: $uname, email: $email})",
+                    {
+                        uname: uname,
+                        email: email,
+                    }
+                );
+            } catch (e) {
+                console.log(e);
+                throw DB_FAILURE;
+            }
+        });
+    } catch (e) {
+        return e == DB_FAILURE
+            ? DB_FAILURE
+            : {
+                  status: 500,
+                  message: (e as Error).message,
+              };
+    }
+    return {
+        status: 200,
+        message: `User ${uname} registered!`,
+    };
 }
 
 async function unregister(uname: string): Promise<RegisterResult> {
-  const db = await getLoginDb();
-  if (!db) {
-    return {
-      status: 500,
-      message: "Failed to load login db",
-    };
-  }
-  try {
-    const stmt = `SELECT * FROM ${LOGIN_TABLE} WHERE uname=:uname`;
-    const result = await db.get(stmt, {
-      ":uname": uname,
-    });
-    if (!result) {
-      return {
-        status: 400,
-        message: `Username ${uname} not registered...`,
-      };
+    const db = await getLoginDb();
+    if (!db) {
+        return {
+            status: 500,
+            message: "Failed to load login db",
+        };
     }
-  } catch (e) {
-    console.log(e);
-    return DB_FAILURE;
-  }
-  try {
-    const stmt = `DELETE FROM ${LOGIN_TABLE} WHERE uname=:uname`;
-    db.run(stmt, {
-      ":uname": uname,
-    });
-  } catch (e) {
-    console.log(e);
-    return DB_FAILURE;
-  }
-  return {
-    status: 200,
-    message: `User ${uname} unregistered.`,
-  };
+    try {
+        const stmt = `SELECT * FROM ${LOGIN_TABLE} WHERE uname=:uname`;
+        const result = await db.get(stmt, {
+            ":uname": uname,
+        });
+        if (!result) {
+            return {
+                status: 400,
+                message: `Username ${uname} not registered...`,
+            };
+        }
+    } catch (e) {
+        console.log(e);
+        return DB_FAILURE;
+    }
+    try {
+        const stmt = `DELETE FROM ${LOGIN_TABLE} WHERE uname=:uname`;
+        db.run(stmt, {
+            ":uname": uname,
+        });
+    } catch (e) {
+        console.log(e);
+        return DB_FAILURE;
+    }
+    return {
+        status: 200,
+        message: `User ${uname} unregistered.`,
+    };
 }
