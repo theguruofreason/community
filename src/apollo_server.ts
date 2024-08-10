c55aa16fe1d849289ad05cbcaf58cd624951e3ee
James Iden Busia <iden@ibusia.me>
2024-08-10 11:16:09 -0700

    WIP GraphQL

--- src/apollo_server.ts
+++ src/apollo_server.ts
@@ -2,17 +2,14 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { Neo4jGraphQL } from '@neo4j/graphql';
 import { Neo4JDriver } from 'db';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 const { APOLLO_PORT } = process.env;
 
-const typeDefs = `#graphql
-    type Person {
-        uname: String
-        email: String
-        relationships: [Person!]! @relationship(type: Family)
-    }
-`
+const GraphQL_Loader = new GraphQLFileLoader();
+const typeDefs = GraphQL_Loader.loadSync("./types.gql", {})[0].rawSDL!;
 
-async function startNeo4JGraphQL(neo4JDriver: Neo4JDriver) {
+
+async function startNeo4JGraphQL(neo4JDriver: Neo4JDriver): Promise<void> {
     const neoSchema: Neo4jGraphQL = new Neo4jGraphQL({typeDefs, driver: neo4JDriver.getDriver()})
     const server = new ApolloServer({
         schema: await neoSchema.getSchema(),
