e2ea5aca143b27deea871dcfe5443070de9db867
James Iden Busia <iden@ibusia.me>
2024-10-05 21:53:11 -0700

    cleanup and add license text to files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,14 @@
+/*
+Copyright 2024, James Iden Busia
+
+This file is part of Community.
+
+Community is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+Community is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Community. If not, see <https://www.gnu.org/licenses/>. 
+*/
 import { Router, Request, Response } from "express";
 import path from "path";
 import { router as login } from "login";
