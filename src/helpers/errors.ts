a80fb28f9f83948d5a8427b48e511b7c68c544dc
James Iden Busia <iden@ibusia.me>
2025-01-28 23:22:20 -0800

    remove next from error handler

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -1,10 +1,10 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
 export interface IErrorWithStatus extends Error {
     readonly status: number;
 }
 
-export const ErrorHandler = (err: Error | IErrorWithStatus, req: Request, res: Response, next: NextFunction): void => {
+export const ErrorHandler = (err: Error | IErrorWithStatus, req: Request, res: Response): void => {
     req.log.error(err, err.message);
     if ('status' in err) {
         res.status(err.status);
